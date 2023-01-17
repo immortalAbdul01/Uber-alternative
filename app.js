69ab14f55cb7467be0aff7dc0b564758ee336d5e
Immortal <ak2061435@gmail.com>
2023-01-17 15:53:24 +0530

    connected with database

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hii how are you doing ')
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+// const { config } = require('process');
+const dotenv = require('dotenv')
+app.listen(2000, () => {
+    console.log('listening on port 2000 ');
+})
+
+dotenv.config({
+    path: './config.env'
+})
+mongoose
+    .connect('mongodb+srv://abdulkhan01:root@trips.yl7b5c0.mongodb.net/?retryWrites=true&w=majority', {
+        // useNewUrlParser: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false,
+        // useUnifiedTopology: true
+    }).then(con => {
+
+        console.log('connected with moongoses');
+    })
