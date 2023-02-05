d294e6b7b9fea594e8aa53cbe66309fc06953a79
Immortal <ak2061435@gmail.com>
2023-02-05 21:21:27 +0530

    changed

--- public/uber-app/src/App.js
+++ public/uber-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './header/Navbar';
+import Ride from './header/Ride';
 
 function App() {
   return (
     <div className="App">
 
       <Navbar />
+      <Ride />
     </div>
   );
 }
