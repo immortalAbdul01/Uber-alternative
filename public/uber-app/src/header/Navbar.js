4564feb2d0659c6ff8c38632e82b54b38a100c7b
khadim <k.hussain1051@gmail.com>
2023-02-05 19:32:15 +0500

    added a navbar in Uber app

--- public/uber-app/src/header/Navbar.js
+++ public/uber-app/src/header/Navbar.js
@@ -1,16 +1,39 @@
 import React from 'react'
-
+import "./Navbar.css"
+import { FiGlobe, FiGrid } from "react-icons/fi";
 export default function Navbar() {
   return (
     <>
-    <div>
+    <div className='main-nav'>
+      <div className="logo">
+      <h1><span>u</span>ber</h1>
+      </div>
+      
+      <div className="menu-links">
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li><a href='/'>Company</a></li>
+        <li><a href='/'>Safety</a></li>
+        <li><a href='/'>Help</a></li>
+       
       </ul>
+  
+      </div>
+      <div className="social-links">
+      <ul>
+        <li><i className='G'><FiGlobe/></i><a href='/'>EN</a></li>
+        <li><i className='G'><FiGrid/></i><a href='/'>Products</a></li>
+        <li><a href='/'>Login</a></li>
+        <li><a href='/'>Sign up</a></li>
+       
+      </ul>
+     </div>
+
+
+
+    
     </div>
+      
+
     </>
   )
 }
--- routes/rideRoutes.js
+++ routes/rideRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const app=express();
 const rideController = require('./../controllers/rideController')
 const driverController = require('./../controllers/driverController')
 const authController = require('./../controllers/authController')
-const router = express.Router()
+const router = app.Router()
 router.post('/book', rideController.bookRide)
 router.post('/becomeDriver', authController.protect, driverController.becomeDriver)
 module.exports = router
